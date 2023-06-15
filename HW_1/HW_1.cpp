ea43a454652f27ae5307b8aaccd57a6978ffe09b
Kammirer <kammirer777@gmail.com>
2023-06-16 00:46:05 +0300

    Add Home work 1

---
+++ HW_1/HW_1.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I Love C++!\n";
+    std::cout << "\n";
+    std::cout << "*\n" << "**\n" << "***\n" << "****\n" << "*****\n" << "******\n" << "*******\n";
+
+    return 0;
+}
