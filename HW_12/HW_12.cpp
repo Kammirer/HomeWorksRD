e7fda9c1ef960fffb080d0e0ec3170cc38dbe200
Kammirer <kammirer777@gmail.com>
2023-08-10 23:26:23 +0300

    Add changes Home work 12

--- HW_12/HW_12.cpp
+++ HW_12/HW_12.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>
 
 const int COLUMN = 3;
 
-enum class SortingDirection {byRows, byColumn};
+enum class SortingDirection { byRows, byColumn };
 
 void printArr(int arr[], int size) {
     for (int i = 0; i < size; i++) {
@@ -20,7 +21,7 @@ void printArr(int arr[][COLUMN], int row) {
 }
 
 void bubleSort(int arr[], int size) {
-    for (int i = 0; i < size-1; i++) {
+    for (int i = 0; i < size - 1; i++) {
         bool sortMark = false;
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -34,71 +35,31 @@ void bubleSort(int arr[], int size) {
     }
 }
 
-void quickSort(int arr[][COLUMN], int left, int right, SortingDirection direction) {
-    if (left >= right) {
-        return;
-    }
-
-    int pivot;
-
-    if (direction == SortingDirection::byRows) {
-        pivot = arr[(left + right) / 2][0];
-    }
-    else {
-        pivot = arr[0][(left + right) / 2];
-    }
-
-    int i = left;
-    int j = right;
-
-    while (i <= j) {
-        if (direction == SortingDirection::byRows) {
-            while (arr[i][0] < pivot) {
-                i++;
-            }
-            while (arr[j][0] > pivot) {
-                j--;
-            }
-        }
-        else {
-            while (arr[0][i] < pivot) {
-                i++;
-            }
-            while (arr[0][j] > pivot) {
-                j--;
-            }
-        }
-
-        if (i <= j) {
-            if (direction == SortingDirection::byRows) {
-                for (int k = 0; k < COLUMN; k++) {
-                    std::swap(arr[i][k], arr[j][k]);
-                }
-            }
-            else {
-                for (int k = 0; k < (right + 1); k++) {
-                    std::swap(arr[k][i], arr[k][j]);
-                }
-            }
-            i++;
-            j--;
-        }
-    }
-
-    if (left < j) {
-        quickSort(arr, left, j, direction);
-    }
-    if (i < right) {
-        quickSort(arr, i, right, direction);
+void sortRows(int arr[][COLUMN], int rows) {
+    for (int i = 0; i < rows; ++i) {
+        std::sort(arr[i], arr[i] + COLUMN);
     }
 }
 
-void sort(int arr[][COLUMN], int row, SortingDirection direction) {
+void sortColumns(int arr[][COLUMN], int rows) {
+    for (int j = 0; j < COLUMN; ++j) {
+        int column[COLUMN];
+        for (int i = 0; i < rows; ++i) {
+            column[i] = arr[i][j];
+        }
+        std::sort(column, column + rows);
+        for (int i = 0; i < rows; ++i) {
+            arr[i][j] = column[i];
+        }
+    }
+}
+
+void sort(int arr[][COLUMN], int rows, SortingDirection direction) {
     if (direction == SortingDirection::byRows) {
-        quickSort(arr, 0, row - 1, SortingDirection::byRows);
+        sortRows(arr, rows);
     }
     else {
-        quickSort(arr, 0, COLUMN - 1, SortingDirection::byColumn);
+        sortColumns(arr, rows);
     }
 }
 
@@ -130,13 +91,13 @@ int main()
     std::cout << "Original 2D array:" << std::endl;
     printArr(arrQuickSort, sizeRow);
     std::cout << std::endl;
-    
+
     sort(arrQuickSort, sizeRow, SortingDirection::byRows);
 
     std::cout << "2D array after sort by rows:" << std::endl;
     printArr(arrQuickSort, sizeRow);
     std::cout << std::endl;
-    
+
     sort(arrQuickSort, sizeRow, SortingDirection::byColumn);
 
     std::cout << "2D array after sort by columns:" << std::endl;
@@ -145,7 +106,7 @@ int main()
 
     // Step 3
     std::cout << "Step 3: time/space complexity by big-O\n\n";
-    
+
     std::cout << "3.1. github.com/robot-dreams-code/UA_C_PLUS_PLUS-FOR-GAME-DEVELOPMENT-01/blob/main/Lecture12/HW3_Code/HW3_Code.cpp:\n";
     std::cout << "= time complexity: O(ROWS * log COLUMNS)\n";
     std::cout << "= space complexity: O(1)\n";
